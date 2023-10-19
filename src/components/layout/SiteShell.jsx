0ff7662962eebea4dd2a88a9a51174730cbd5f4c
manuelsdev <masanchezzm@gmail.com>
2023-10-19 09:36:30 +0200

    main background image with svg logo

--- src/components/layout/SiteShell.jsx
+++ src/components/layout/SiteShell.jsx
@@ -8,6 +8,7 @@ import StaticSidebar from './StaticSidebar'
 import React from 'react'
 import SiteAppBar from './site/SiteAppBar'
 import Image from 'next/image'
+import IconCorpName from '../svg/IconCorpName'
 //import IconCorpName from '../svg/IconCorpName'
 //import LogoName from '../svg/LogoName'
 
@@ -21,10 +22,6 @@ export default function SiteShell({ children, sidebarContent }) {
             sidebarContent={sidebarContent}
          />
 
-
-
-         
-
          {/* Static sidebar for desktop
           <StaticSidebar sidebarContent={sidebarContent} />
          */}
@@ -33,9 +30,14 @@ export default function SiteShell({ children, sidebarContent }) {
             <SiteAppBar setSidebarOpen={setSidebarOpen} />
 
             <main>
-               <div className="bg-home-main min-h-[calc(100vh-64px)] w-full bg-cover bg-[70%] bg-no-repeat min-[900px]:bg-center">
-                  <div className="flex h-full justify-center brightness-50">
-
+               <div className="min-h-[calc(100vh-64px)] w-full bg-home-main bg-cover bg-fixed bg-[70%] bg-no-repeat min-[900px]:bg-center">
+                  <div className="flex h-full justify-center backdrop-brightness-75">
+                     <IconCorpName
+                        className={
+                           'h-screen w-full max-w-[1000px] fill-[#D5FF40] stroke-white'
+                        }
+                        viewBox={'0 0 654 97'}
+                     />
                   </div>
                </div>
                <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
