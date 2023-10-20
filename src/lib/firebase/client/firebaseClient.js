df21359343ab4833b3cf5b4a6fd0b680d6db0c1f
manuelsdev <masanchezzm@gmail.com>
2023-10-20 18:33:52 +0200

    firebase client

---
+++ src/lib/firebase/client/firebaseClient.js
@@ -0,0 +1,28 @@
+import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getAnalytics } from 'firebase/analytics'
+//import Firebase from 'firebase'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+   authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
+   projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+   storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
+   messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
+   appId: process.env.NEXT_PUBLIC_FB_APP_ID,
+   measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENT_ID,
+}
+
+// Initialize Firebase
+
+//console.log(getApps())
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+//console.log(getApp())
+//console.log(getApps())
+//console.log(app)
+//const analytics = getAnalytics(app)
+//export const auth = getAuth(app)
