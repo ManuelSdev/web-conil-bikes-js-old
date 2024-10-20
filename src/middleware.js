206d805a4698bb94b8076b76044ab945e8ac6cbe
manuelsdev <masanchezzm@gmail.com>
2024-10-20 12:46:37 +0200

    *

--- src/middleware.js
+++ src/middleware.js
@@ -54,7 +54,7 @@ export async function middleware(request) {
       const nextUrlHeader = headersList.get('next-url')
       //console.log('refererHeader -> ', refererHeader)
       //console.log('nextUrlHeader -> ', nextUrlHeader)
-      /*
+
       const response = await authMiddleware({
          isAdmin: true,
          request,
@@ -62,19 +62,18 @@ export async function middleware(request) {
 
          resolvedUrl,
       })
-      // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~', request.nextUrl)
+      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~', request.nextUrl)
       if (response) return response
-      // return NextResponse.next()
-      
-     // if (pathName.includes('/new/bikes' || '/new/address' || '/new/resume')) {
-     //    const response = datedStepperMiddleware({
-     //       request,
-     //       NextResponse,
-      //      urlToRedirect: '/dashboard/bookings/new/date' + searchPath,
-      //   })
-      //   if (response) return response
-     // }
-      */
+      return NextResponse.next()
+
+      if (pathName.includes('/new/bikes' || '/new/address' || '/new/resume')) {
+         const response = datedStepperMiddleware({
+            request,
+            NextResponse,
+            urlToRedirect: '/dashboard/bookings/new/date' + searchPath,
+         })
+         if (response) return response
+      }
    }
 
    if (
